7d59ba0b506aa82b2fccf11a7df9007c759665f2
mperleo <tiomakelelle@outlook.com>
2021-11-02 17:50:05 +0100

    ficheros iniciales

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/mdb.dark.min.css">
+        <link rel="stylesheet" href="css/mdb.min.css">
+        <link rel="stylesheet" href="css/mdb.rtl.min.css">
+        <title>SOA Practica services consumer</title>
+    </head>
+
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light nav-tabs border-bottom border-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Services Customer</a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Datos de demanda eléctrica</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Analisis de datos de la junta</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container-fluid mt-4 bg-info">
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col">
+                    <div class="card border border-dark mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Demanda electrica</h5>
+                            <p class="card-text">
+                            Datos de la demanda electrica en España
+                            </p>
+                            <button type="button" class="btn btn-outline-dark">ir</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card border border-dark mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Puntos de vehiculos electricos</h5>
+                            <p class="card-text">
+                            Datos de puntos de recarga de vehiculos electricos en Castilla y León en fechas dadas
+                            </p>
+                            <button type="button" class="btn btn-outline-dark">ir</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card border border-dark mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Convocatoria de empleo público</h5>
+                            <p class="card-text">
+                            Datos de las convocatorias de empleo publico en castilla y leon entre fechas dadas
+                            </p>
+                            <button type="button" class="btn btn-outline-dark">ir</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card border border-dark mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Establecimientos farmacéuticos</h5>
+                            <p class="card-text">
+                            Datos de establecimientos farmacéuticosen Castilla y León entre fechas dadas
+                            </p>
+                            <button type="button" class="btn btn-outline-dark">ir</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card border border-dark mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">Tasa de mortalidad COVID-19 por zonas de salud</h5>
+                            <p class="card-text">
+                            Datos de tasa de mortalidad COVID-19 por zonas de salud de Castilla y León entre fechas dadas
+                            </p>
+                            <button type="button" class="btn btn-outline-dark">ir</button>
+                        </div>
+                    </div>
+                </div>  
+
+            </div>
+        </div>
+    </body>
+
+<script src="js/mdb.min.js"></script>
+</html>
